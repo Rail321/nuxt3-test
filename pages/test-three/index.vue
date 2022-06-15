6d674b552fc433585c467a90379d22a63859ee7b
unknown <shakurov-98@mail.ru>
2022-06-15 17:54:11 +0300

    +add

---
+++ pages/test-three/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    {{ todoList }}
+  </div>
+</template>
+
+<script setup>
+  const { data: todoList } = useAsyncData( 'todoList', () => $fetch( 'https://jsonplaceholder.typicode.com/todos?_limit=7' ) )
+</script>
